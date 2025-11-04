1067a19aefaa41b8f2fdbf6ecb5b453446ad4b5a
Liliya2727 <syarifbintang16@gmail.com>
2025-11-04 14:16:15 +0700

    modify module.prop

--- .github/scripts/compile_zip.sh
+++ .github/scripts/compile_zip.sh
@@ -47,7 +47,7 @@ cp LICENSE ./mainfiles
 find mainfiles/system/bin -maxdepth 1 -type f -name "*.sh" -exec sh -c 'mv -- "$0" "${0%.sh}"' {} \;
 
 # Parse version info to module prop
-zipName="AZenith-$version-$release_code.zip"
+zipName="AZenithEunoia-$version-$release_code.zip"
 echo "zipName=$zipName" >>"$GITHUB_OUTPUT"
 
 # Generate sha256sum for integrity checkup
