01d7906a562d21cd15128f81b4f1ab44ebd482cf
Liliya2727 <syarifbintang16@gmail.com>
2025-12-13 10:06:12 +0700

    /

--- .github/scripts/verify.sh
+++ .github/scripts/verify.sh
@@ -15,10 +15,10 @@ cd "$GITHUB_WORKSPACE" || {
 
 # Write module version to daemon and webui
 version="$(cat version)"
-release_code="$(git rev-list HEAD --count)-$(git rev-parse --short HEAD)-release"
+release_code="$(git rev-list HEAD --count)-$(git rev-parse --short HEAD)-experimental"
 echo "start sending AZenith version: $version ($release_code) to WebUI and Daemon"
 sed -i "s|#define MODULE_VERSION \".*\"|#define MODULE_VERSION \"$version ($release_code)\"|" jni/include/AZenith.h
 sed -i 's#const WEBUI_VERSION = ".*";#const WEBUI_VERSION = "'"$version ($release_code)"'";#' webui/src/scripts/webui_utils.js
 
-echo "$(cat webui/src/scripts/webui_utils.js | grep 'const WEBUI_VERSION')"
-echo "$(cat jni/include/AZenith.h | grep MODULE_VERSION)"
+echo "Successfully write to webui_utils.json: $(cat webui/src/scripts/webui_utils.js | grep 'const WEBUI_VERSION')"
+echo "Successfully write to AZenith.h: $(cat jni/include/AZenith.h | grep MODULE_VERSION)"
